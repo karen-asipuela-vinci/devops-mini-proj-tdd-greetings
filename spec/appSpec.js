86835958e470091e1e2156d825d3c2ed05b25c9a
samxerus <gilles.dejamblinne@student.vinci.be>
2023-11-12 15:02:21 +0100

    add: When name is null

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -3,9 +3,15 @@ import { Main } from '../src/helloWordSpec.js';
 describe("A suite of test for the demo function of the main class", function() {
     let main = new Main();
 
-    it("return Hello, {name} when given name ", function() {
+    it("return Hello, {name}. when given name ", function() {
         const result = main.greet("Jean-Kevin");
         expect(result).toBe("Hello, Jean-Kevin.");
+
+    });
+
+    it("return Hello, my friend when given null ", function() {
+        const result = main.greet(null);
+        expect(result).toBe("Hello, my friend.");
 
     });
 
--- src/helloWordSpec.js
+++ src/helloWordSpec.js
@@ -1,6 +1,7 @@
 export class Main {
 
     greet(name) {
+       if(name == null) return "Hello, my friend.";
         return "Hello, " + name +".";
     }
     
