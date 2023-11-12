6cd72388d217949fc33da2e8e85da09705b0332d
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2023-11-12 14:48:34 +0100

    update name spec jasmine

---
+++ spec/appSpec.js
@@ -0,0 +1,12 @@
+import { Main } from '../src/helloWord.js';
+
+describe("A suite of test for the demo function of the main class", function() {
+    let main = new Main();
+
+    it("return Hello, {name} when given name ", function() {
+        const result = main.greet("Jean-Kevin");
+        expect(result).toBe("Hello, Jean-Kevin.");
+
+    });
+
+});
