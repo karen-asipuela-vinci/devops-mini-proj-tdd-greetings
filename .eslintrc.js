6684839ba63a73bfee505d8eb52135d256011797
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2023-11-16 22:48:32 +0100

    correction in .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['standard'],
+  env: {
+    browser: true,
+    es2021: true
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    semi: ['error', 'always']
+  }
+};
