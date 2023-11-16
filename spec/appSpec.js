cb625e0ce6cf2db2412518d9b12e3bc23651d351
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2023-11-16 22:52:11 +0100

    app eslint and prettier fix

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,73 +1,75 @@
 /* eslint-disable no-undef */
 
-import { Main } from '../src/helloWordSpec.js'
+import { Main } from '../src/helloWordSpec.js';
 
- describe('A suite of test for the demo function of the main class', function () {
-    let main = new Main()
+describe('A suite of test for the demo function of the main class', function () {
+  const main = new Main();
 
-    it('return Hello, {name}. when given name ', function () {
-        const result = main.greet('Jean-Kevin')
+  it('return Hello, {name}. when given name ', function () {
+    const result = main.greet('Jean-Kevin');
 
-        expect(result).toBe('Hello, Jean-Kevin.')
-    })
+    expect(result).toBe('Hello, Jean-Kevin.');
+  });
 
-    it('return Hello, my friend when given null ', function () {
-        const result = main.greet(null)
+  it('return Hello, my friend when given null ', function () {
+    const result = main.greet(null);
 
-          expect(result).toBe('Hello, my friend.')
-    })
+    expect(result).toBe('Hello, my friend.');
+  });
 
-    it('return Hello, my friend when given undefined ', function () {
-        const result = main.greet(undefined)
+  it('return Hello, my friend when given undefined ', function () {
+    const result = main.greet(undefined);
 
-        expect(result).toBe('Hello, my friend.')
-    })
+    expect(result).toBe('Hello, my friend.');
+  });
 
-    it('return Hello, my friend when given empty name ', function () {
-        const result = main.greet(' ')
+  it('return Hello, my friend when given empty name ', function () {
+    const result = main.greet(' ');
 
-        expect(result).toBe('Hello, my friend.')
-    })
+    expect(result).toBe('Hello, my friend.');
+  });
 
-    it('return HELLO, KENOBI! when given KENOBI ', function () {
-        const result = main.greet('KENOBI')
+  it('return HELLO, KENOBI! when given KENOBI ', function () {
+    const result = main.greet('KENOBI');
 
-        expect(result).toBe('HELLO, KENOBI!')
-    })
+    expect(result).toBe('HELLO, KENOBI!');
+  });
 
-    it('return Hello, Kratos and Thanathos. when given Kratos, Thanathos', function () {
-        const result = main.greet(['Kratos', 'Thanathos'])
+  it('return Hello, Kratos and Thanathos. when given Kratos, Thanathos', function () {
+    const result = main.greet(['Kratos', 'Thanathos']);
 
-        expect(result).toBe('Hello, Kratos and Thanathos.')
-    })
+    expect(result).toBe('Hello, Kratos and Thanathos.');
+  });
 
-    it('return Hello, Kratos, Thanathos and Hypnos. when given Kratos, Thanathos, Hypnos', function () {
-        const result = main.greet(['Kratos', 'Thanathos', 'Hypnos'])
+  it('return Hello, Kratos, Thanathos and Hypnos. when given Kratos, Thanathos, Hypnos', function () {
+    const result = main.greet(['Kratos', 'Thanathos', 'Hypnos']);
 
-        expect(result).toBe('Hello, Kratos, Thanathos and Hypnos.')
-    })
+    expect(result).toBe('Hello, Kratos, Thanathos and Hypnos.');
+  });
 
-    it('return Hello, Thanathos and Hypnos. AND HELLO KRATOS! when given KRATOS, Thanathos, Hypnos', function () {
-        const result = main.greet(['KRATOS', 'Thanathos', 'Hypnos'])
+  it('return Hello, Thanathos and Hypnos. AND HELLO KRATOS! when given KRATOS, Thanathos, Hypnos', function () {
+    const result = main.greet(['KRATOS', 'Thanathos', 'Hypnos']);
 
-        expect(result).toBe('Hello, Thanathos and Hypnos. AND HELLO KRATOS!')
-    })
+    expect(result).toBe('Hello, Thanathos and Hypnos. AND HELLO KRATOS!');
+  });
 
-    it('return Hello, Thanathos and Hypnos. AND HELLO KRATOS! when given KRATOS, THOR, Thanathos, Hypnos', function () {
-        const result = main.greet(['KRATOS','THOR', 'Thanathos', 'Hypnos'])
+  it('return Hello, Thanathos and Hypnos. AND HELLO KRATOS! when given KRATOS, THOR, Thanathos, Hypnos', function () {
+    const result = main.greet(['KRATOS', 'THOR', 'Thanathos', 'Hypnos']);
 
-        expect(result).toBe('Hello, Thanathos and Hypnos. AND HELLO KRATOS AND THOR!')
-    })
+    expect(result).toBe(
+      'Hello, Thanathos and Hypnos. AND HELLO KRATOS AND THOR!'
+    );
+  });
 
-    it('return Bonjour Kratos et Thanatos. when given Kratos, thanathos, fr', function () {
-        const result = main.greet(['kratos', 'Thanathos', 'fr'])
+  it('return Bonjour Kratos et Thanatos. when given Kratos, thanathos, fr', function () {
+    const result = main.greet(['kratos', 'Thanathos', 'fr']);
 
-        expect(result).toBe('Bonjour, kratos et Thanathos.')
-    })
+    expect(result).toBe('Bonjour, kratos et Thanathos.');
+  });
 
-    it('return Bonjour Kratos. ET BONJOUR THANATHOS! when given Kratos, thanathos, fr', function () {
-        const result = main.greet(['Kratos', 'THANATHOS', 'fr'])
+  it('return Bonjour Kratos. ET BONJOUR THANATHOS! when given Kratos, thanathos, fr', function () {
+    const result = main.greet(['Kratos', 'THANATHOS', 'fr']);
 
-        expect(result).toBe('Bonjour, Kratos. ET BONJOUR THANATHOS!')
-    })
-})
+    expect(result).toBe('Bonjour, Kratos. ET BONJOUR THANATHOS!');
+  });
+});
--- src/helloWordSpec.js
+++ src/helloWordSpec.js
@@ -1,115 +1,115 @@
 export class Main {
-    greet(name) {
-        if (name == null || name == ' ') return 'Hello, my friend.'
-        if (Array.isArray(name)) {
-            if (name.length > 1) {
-                let upperCase = []
-                let lowerCase = []
-                let langue = null
-                name.forEach((element) => {
-                    if (
-                        element === 'fr' ||
+  greet (name) {
+    if (name === null || name === ' ') return 'Hello, my friend.';
+    if (Array.isArray(name)) {
+      if (name.length > 1) {
+        const upperCase = [];
+        const lowerCase = [];
+        let langue = null;
+        name.forEach((element) => {
+          if (
+            element === 'fr' ||
                         element === 'en' ||
                         element === 'nl'
-                    ) {
-                        langue = element
-                    } else if (element.toUpperCase() == element) {
-                        upperCase.push(element)
-                    } else {
-                        lowerCase.push(element)
-                    }
-                })
+          ) {
+            langue = element;
+          } else if (element.toUpperCase() === element) {
+            upperCase.push(element);
+          } else {
+            lowerCase.push(element);
+          }
+        });
 
-                let printLower
-                if (lowerCase.length === 0) {
-                    printLower = ''
-                } else {
-                    if (langue != null) {
-                        if (langue == 'fr') {
-                            printLower = 'Bonjour, ' + lowerCase[0]
-                        } else if (langue == 'nl') {
-                            printLower = 'Goeiedag, ' + lowerCase[0]
-                        }
-                    } else {
-                        printLower = 'Hello, ' + lowerCase[0]
-                    }
-                }
+        let printLower;
+        if (lowerCase.length === 0) {
+          printLower = '';
+        } else {
+          if (langue != null) {
+            if (langue === 'fr') {
+              printLower = 'Bonjour, ' + lowerCase[0];
+            } else if (langue === 'nl') {
+              printLower = 'Goeiedag, ' + lowerCase[0];
+            }
+          } else {
+            printLower = 'Hello, ' + lowerCase[0];
+          }
+        }
 
-                if (lowerCase.length > 1) {
-                    const notLast = lowerCase.slice(1, -1)
-                    const last = lowerCase.slice(-1)
-                    notLast.forEach((element) => {
-                        printLower += ', ' + element
-                    })
-                    if (langue != null) {
-                        if (langue == 'fr') {
-                            printLower = printLower + ' et ' + last + '.'
-                        } else if (langue == 'nl') {
-                            printLower = printLower + ' en ' + last + '.'
-                        }
-                    } else {
-                        printLower = printLower + ' and ' + last + '.'
-                    }
-                } else {
-                    printLower += '.'
-                }
+        if (lowerCase.length > 1) {
+          const notLast = lowerCase.slice(1, -1);
+          const last = lowerCase.slice(-1);
+          notLast.forEach((element) => {
+            printLower += ', ' + element;
+          });
+          if (langue != null) {
+            if (langue === 'fr') {
+              printLower = printLower + ' et ' + last + '.';
+            } else if (langue === 'nl') {
+              printLower = printLower + ' en ' + last + '.';
+            }
+          } else {
+            printLower = printLower + ' and ' + last + '.';
+          }
+        } else {
+          printLower += '.';
+        }
 
-                if (upperCase.length === 0) {
-                    if (langue != null) {
-                        if (langue == 'fr') {
-                            return printLower
-                        } else if (langue == 'nl') {
-                            return printLower
-                        }
-                    } else {
-                        return printLower
-                    }
-                }
-                if (upperCase.length === 1) {
-                    if (langue != null) {
-                        if (langue == 'fr') {
-                            return (
-                                printLower + ' ET BONJOUR ' + upperCase[0] + '!'
-                            )
-                        } else if (langue == 'nl') {
-                            return (
-                                printLower +
+        if (upperCase.length === 0) {
+          if (langue != null) {
+            if (langue === 'fr') {
+              return printLower;
+            } else if (langue === 'nl') {
+              return printLower;
+            }
+          } else {
+            return printLower;
+          }
+        }
+        if (upperCase.length === 1) {
+          if (langue != null) {
+            if (langue === 'fr') {
+              return (
+                printLower + ' ET BONJOUR ' + upperCase[0] + '!'
+              );
+            } else if (langue === 'nl') {
+              return (
+                printLower +
                                 ' EN GOEIENDAG ' +
                                 upperCase[0] +
                                 '!'
-                            )
-                        }
-                    } else {
-                        return printLower + ' AND HELLO ' + upperCase[0] + '!'
-                    }
-                }
-                let printUpper 
-                if (langue != null) {
-                    if (langue == 'fr') {
-                        printUpper = ' ET BONJOUR, ' + upperCase[0]
-                    } else if (langue == 'nl') {
-                        printUpper = ' EN GOEIENDAG, ' + upperCase[0]
-                    }
-                } else {
-                    printUpper = ' AND HELLO ' + upperCase[0]
-                }
-                const notLastU = upperCase.slice(1, -1)
-                const lastU = upperCase.slice(-1)
-                notLastU.forEach((element) => {
-                    printUpper += ', ' + element
-                })
-                if (langue != null) {
-                    if (langue == 'fr') {
-                        return printLower + printUpper + ' ET' + lastU + '!'
-                    } else if (langue == 'nl') {
-                        return printLower + printUpper + ' EN' + lastU + '!'
-                    }
-                } else {
-                    return printLower + printUpper + ' AND ' + lastU + '!'
-                }
+              );
             }
+          } else {
+            return printLower + ' AND HELLO ' + upperCase[0] + '!';
+          }
         }
-        if (name == name.toUpperCase()) return 'HELLO, ' + name + '!'
-        return 'Hello, ' + name + '.'
+        let printUpper;
+        if (langue != null) {
+          if (langue === 'fr') {
+            printUpper = ' ET BONJOUR, ' + upperCase[0];
+          } else if (langue === 'nl') {
+            printUpper = ' EN GOEIENDAG, ' + upperCase[0];
+          }
+        } else {
+          printUpper = ' AND HELLO ' + upperCase[0];
+        }
+        const notLastU = upperCase.slice(1, -1);
+        const lastU = upperCase.slice(-1);
+        notLastU.forEach((element) => {
+          printUpper += ', ' + element;
+        });
+        if (langue != null) {
+          if (langue === 'fr') {
+            return printLower + printUpper + ' ET' + lastU + '!';
+          } else if (langue === 'nl') {
+            return printLower + printUpper + ' EN' + lastU + '!';
+          }
+        } else {
+          return printLower + printUpper + ' AND ' + lastU + '!';
+        }
+      }
     }
+    if (name === name.toUpperCase()) return 'HELLO, ' + name + '!';
+    return 'Hello, ' + name + '.';
+  }
 }
